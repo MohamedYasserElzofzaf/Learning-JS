2ffe2ac348f2886fa4e2daa005c4a102d946565f
Mohamed Yasser <Mohammed.yasser.elzofzaf@gmail.com>
2020-11-23 21:26:59 +0200

    Added javascript files
    functions and function return statments

--- Functions.js
+++ Functions.js
@@ -1,8 +1,52 @@
+// food price
 var coca_cost = 5;
 var burger_cost = 35;
 var fries_cost = 12 ;
+// user's budget
+var account_balance = 500 ;
 
+function check_balance(amount){
+    if( account_balance - amount >= 0){
+            return true;
+    }else{
+            console.log("your balance is empty");
+            return false;
+    }
+}
 
+function eat_burger(){
+    if(check_balance(burger_cost)){
+        account_balance = account_balance - burger_cost ;
+        console.log("Enjoy your burger");
+    }
+    console.log("\t Balance : ", account_balance);
+}
+function eat_fries(){
+    if(check_balance(fries_cost)){
+        account_balance = account_balance - fries_cost ;
+        console.log("Enjoy your fries");
+    }
+    console.log("\t Balance : ",account_balance);
+}
+function drink_coca(){
+    if(check_balance(coca_cost)){
+        account_balance = account_balance - coca_cost ;
+        console.log("Enjoy your coca ");
+    }    
+    console.log("\t Balance : ", account_balance);
+}
 
-console.log();
-document.getElementById('test').innerHTML=rmfc;
+//eat_burger();
+//eat_fries();
+//drink_coca();
+//document.getElementById('test').innerHTML=eat_fries();
+
+var MealList = [eat_burger , eat_fries , drink_coca ];
+function visit(mlist){
+    for(var i=0 , num = mlist.length ; i< num ; i++){
+       // mlist[i]();
+        mlist[i].call();
+    }
+}
+visit(MealList);
+//document.getElementById('test').innerHTML=visit(MealList);
