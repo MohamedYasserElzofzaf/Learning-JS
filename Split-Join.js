874e5f2fcd68c95f8be8930de7e11fcbce3b1d00
Mohamed Yasser <Mohammed.yasser.elzofzaf@gmail.com>
2020-11-25 21:25:33 +0200

    knows new functions

---
+++ Split-Join.js
@@ -0,0 +1,14 @@
+// to convert from string to array use split()
+// to convert from array to string use join()
+let name ="hello word my name is yasser and i want to be a js devolper";
+let x = name.split(' ');
+console.log(x);
+
+let y = name.split(' ').sort().join('');
+console.log(y);
+
+
+
+
+// document.getElementById('test').innerHTML;
+// document.write(x);
