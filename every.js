c59299cf73429f12e66c44859faf77e30193e325
Mohamed Yasser <Mohammed.yasser.elzofzaf@gmail.com>
2020-12-03 11:08:01 +0200

    modife every() method

--- every.js
+++ every.js
@@ -2,12 +2,12 @@ let numbers=[33,55,22,98,11,19,84];
 
 let greaterthan20 = numbers.every(function(num){
     return num > 20;
-})
+});
 let greaterthan10 = numbers.every(function(num){
     return num > 10;
-})
+});
 console.log(greaterthan20);
-console.log('\n' greaterthan10);
+console.log(greaterthan10);
 document.getElementById('test').innerHTML;
 document.write(greaterthan20);
 document.write(greaterthan10);
