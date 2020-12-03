75aa95c725d3e7de9f23a047c7aafa207554985e
Mohamed Yasser <Mohammed.yasser.elzofzaf@gmail.com>
2020-12-03 14:44:56 +0200

    adding function currying javaScript

---
+++ functions-currying.js
@@ -0,0 +1,18 @@
+
+// Function Currying
+//In JavaScript, functions are first-class objects, 
+// just like String, Number, Boolean
+//This means that they can be passed to functions or returned from functions
+
+function greet(msg){
+    return function (name){
+        console.log(msg , name);
+    }
+}
+let english = greet(" Hi ");
+let spainish = greet(' Hola ');
+let deutsch = greet(' Tag ');
+
+english('Tom');
+spainish('Sergio');
+deutsch('Van')
