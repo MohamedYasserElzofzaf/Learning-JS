52efcbf475aefa7f43fc2c0f54839a9db3d3aa3e
Mohamed Yasser <Mohammed.yasser.elzofzaf@gmail.com>
2020-12-05 11:50:23 +0200

    add event listeners

--- event-listeners.js
+++ event-listeners.js
@@ -1,3 +1,31 @@
 // JavaScript Event Listeners
 // Event-driven programming: your program waits for events and uses them as triggers to run the next function(s)
 
+// myDiv.addEventListener('click', doSomething )
+// function doSomething(ev){
+//     console.log(ev.type);
+//     console.log(ev.target);
+// }
+
+let btn = document.getElementById('btn');
+let link = document.getElementById('link');
+let txt = document.getElementById('txt');
+
+btn.addEventListener('click' , buttonclicked)
+function buttonclicked(ev){
+    console.log(ev.type , ev.target , ev.currentTarget);
+}
+link.addEventListener('click' , linkclicked)
+function linkclicked(ev){
+    ev.preventDefault();
+    console.log(ev.type , ev.target , ev.currentTarget);
+}
+txt.addEventListener('input', ev=>{
+    console.log(ev.type , ev.target.value);
+})
+txt.addEventListener('change', ev=>{
+    console.log(ev.type , ev.target.value);
+})
+txt.addEventListener('blur', ev=>{
+    console.log(ev.type , ev.target.value);
+})
