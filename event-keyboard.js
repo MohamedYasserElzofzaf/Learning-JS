906b1447041c28fff8e0e4d6aeba6ee00d020baa
Mohamed Yasser <Mohammed.yasser.elzofzaf@gmail.com>
2020-12-07 16:45:15 +0200

    modify the html file to fit the new JS file

--- event-keyboard.js
+++ event-keyboard.js
@@ -1,4 +1,20 @@
 // Keyboard events in the Browser
 //ev.char || ev.charCode || ev.which
-// keydown keyup keypress
+//keyboard events : keydown, keyup, keypress
 let log = console.log;
+document.addEventListener('DOMContentLoaded' , init); 
+// means when the browsers finsh reading all of html then its gonna call init function
+
+function init(){
+    let txt = document.getElementById('txt');
+    txt.addEventListener('keydown' , anykey);
+}
+function anykey(ev){
+    //log(ev.type , ev.target);
+    let target = ev.currentTarget;
+    let tag = target.tagName;
+    let char = ev.char || ev.charCode || ev.which;
+    log(char, tag);
+    let s = String.fromCharCode(char);
+    log(s);
+}
