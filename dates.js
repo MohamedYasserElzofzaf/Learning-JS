5102260397eaf3c3d0c8c8d2b0102f4ef331b66f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-13 23:27:05 +0200

    working with dates and time in JS

---
+++ dates.js
@@ -0,0 +1,12 @@
+// Date objects in JavaScript
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+let log = console.log;
+
+let d = new Date();
+log(d);
+
+let num = 1000 * 60 * 60 * 24 * 365.25 * 40;
+// approx 40 yrs
+// ms * sec * min * hr * day * year * x
+let str = "20 July 2012";
