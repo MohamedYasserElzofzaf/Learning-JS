25b099d9019606a8b136ee6c75b2df1c350770d6
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-21 18:35:13 +0200

    Null VS Undefined

--- null-undefined.js
+++ null-undefined.js
@@ -7,3 +7,15 @@
 // but hasn't been given a value yet
 // functions with no return statement
 // will return undefined by default
+
+console.log(typeof null); // "object"
+console.log(typeof undefined); // "undefined"
+console.log(null === undefined); // false
+console.log(null == undefined); // true
+console.log(null === null); // true
+console.log(null == null); // true
+console.log(undefined === undefined); //true
+console.log(undefined == undefined); //true
+console.log(!null); // true
+console.log(isNaN(1 + null)); // false
+console.log(isNaN(1 + undefined)); // true
