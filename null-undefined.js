d8632eee7a745144918a0f4f866de444a086ec3b
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-21 18:16:10 +0200

    Null VS Undefined

---
+++ null-undefined.js
@@ -0,0 +1,9 @@
+// null and undefined in Js
+
+// null means nothing, void, emptiness
+// and is intentionally set
+//
+// undefined means the container exists
+// but hasn't been given a value yet
+// functions with no return statement
+// will return undefined by default
