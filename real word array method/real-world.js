57cdd40b768a2d9cf609ecca390ea386ebfed09b
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-22 11:22:48 +0200

    Add the content of the JavaScript file

--- real word array method/real-world.js
+++ real word array method/real-world.js
@@ -5,3 +5,31 @@
 let uri = "./darksky-sample.json";
 let req = new Request(uri, { method: "GET" });
 let container, df;
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    container = document.getElementById("container");
+    df = new DocumentFragment();
+    fetch(req)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("BAD HTTP");
+            }
+        })
+        .then((json) => {
+            json.hourly.data.forEach((hour) => {
+                let div = document.createElement("div");
+                div.classList.add("hour");
+                let span = document.createElement("span");
+                span.textContent = "0:00";
+                div.appendChild(span);
+                df.appendChild(div);
+            });
+            container.appendChild(df);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+}
