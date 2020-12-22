754cb16720b7798fa7d8a4af417e29158e52663f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-22 13:25:57 +0200

    Add objectAssign js file

--- object-assign.js
+++ object-assign.js
@@ -1,12 +1,12 @@
 //Object.assign(target, sources... ) method
 // used to copy objects OR
-
-const objectAssign = require("object-assign");
-
 // to merge objects
 let obj1 = { arms: true, armCount: 2 };
 let obj2 = { weapons: ["missle launcher", "reciprocating saw"] };
 let obj3 = { canMove: true, legs: 0, treads: 2 };
 
-let arms = objectAssign({}, obj1);
+let arms = Object.assign({ hashand: true }, obj1);
 console.log(arms);
+
+let braveheart = Object.assign({}, obj1, obj2, obj3);
+console.log(braveheart);
