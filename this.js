62bebea4dcba9d426798cdb079b689ccc9698add
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-23 13:05:37 +0200

    keyword this in javascript

--- this.js
+++ this.js
@@ -1,7 +1,21 @@
 // this.js
 // Understanding what "this" is
 //
-//"use strict";
+"use strict";
 
 let box = document.querySelector(".box");
-let log = console.log;
+
+let myfunc = function() {
+    console.log("1", this);
+    setTimeout(() => {
+        console.log("3", "timed out", this);
+    }, 250);
+};
+let myfunc1 = () => {
+    console.log("2", this);
+    setTimeout(() => {
+        console.log("3", "timed out", this);
+    }, 250);
+};
+box.addEventListener("mousedown", myfunc);
+box.addEventListener("mouseup", myfunc1);
