3314f706d2d76800aec8189b21b4419e9374c1f0
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2020-12-30 21:47:41 +0200

    JavaScript Double Bang!!

---
+++ double-bang.js
@@ -0,0 +1,10 @@
+let num = 12;
+//if( !num )
+let bool = !!num;
+console.log(bool);
+
+let str = "12";
+let number = +str;
+console.log(typeof str);
+console.log(typeof number);
+console.log(number);
